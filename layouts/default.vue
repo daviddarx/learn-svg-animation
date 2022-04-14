4d25377f6e284bc499a5c7fb851b3f4bb375ee42
David Darx <david.darx@liip.ch>
2022-04-14 10:43:56 +0200

    feat(setup) remove unneed component imports
    auto import is setted in nuxt config

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,17 +8,8 @@
 </template>
 
 <script>
-import AppLogo from '@/components/AppLogo.vue'
-import AppNav from '@/components/AppNav.vue'
-import AppFooter from '@/components/AppFooter.vue'
-
 export default {
   name: 'DefaultLayout',
-  components: {
-    AppLogo,
-    AppNav,
-    AppFooter,
-  },
 }
 </script>
 
