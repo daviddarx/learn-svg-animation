6abb44f68975bec9dfe294f61026d43472c0c83f
David Darx <david.darx@liip.ch>
2022-04-14 11:20:56 +0200

    feat(blog) add blog list and details

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="blog">BLOG</div>
+  <div class="blog">
+    BLOG
+
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+    </li>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'BlogPage',
+  async asyncData({ $content }) {
+    const posts = await $content('blog').fetch()
+
+    return {
+      posts,
+    }
+  },
 }
 </script>
 
