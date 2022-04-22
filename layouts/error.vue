fc3974f038988c811a6520c1e7b049e97d82fa13
David Darx <david.darx@liip.ch>
2022-04-22 11:50:30 +0200

    feat(error) adds layout for error pages

---
+++ layouts/error.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ pageTitle }}</h1>
+
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage',
+  layout: 'default',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    pageTitle() {
+      let title = 'A problem occured'
+
+      if (this.error.statusCode === 404) {
+        title = 'Page not found'
+      } else if (this.error.message) {
+        title = this.error.message
+      }
+
+      return title
+    },
+  },
+}
+</script>
