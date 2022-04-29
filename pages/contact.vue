37e821599416f6aa645ef4eaa28be0fbb70ff2f3
David Darx <david.darx@liip.ch>
2022-04-29 09:39:17 +0200

    feat(contact) implement content

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,12 +1,20 @@
 <template>
   <article class="contact">
-    <h1 class="animate-in">Contact</h1>
+    <h1 class="animate-in">{{ page.title }}</h1>
+    <nuxt-content :document="page" class="animate-in animate-in--s1" />
   </article>
 </template>
 
 <script>
 export default {
   name: 'ContactPage',
+  async asyncData({ $content }) {
+    const page = await $content('contact/contact').fetch()
+
+    return {
+      page,
+    }
+  },
   data() {
     return {
       pageTitle: 'Contact',
