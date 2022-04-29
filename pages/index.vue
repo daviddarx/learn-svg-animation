e6919a12b2661d0d15890076a50967b2f6184c8e
David Darx <david.darx@liip.ch>
2022-04-29 09:31:10 +0200

    feat(home) implement content

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,29 @@
 <template>
   <article class="home">
-    <h1 class="animate-in">Home</h1>
+    <h1 class="animate-in">{{ page.title }}</h1>
+    <nuxt-content :document="page" class="animate-in animate-in--s1" />
   </article>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
+  async asyncData({ $content }) {
+    const page = await $content('home/home').fetch()
+
+    return {
+      page,
+    }
+  },
   head() {
     return {
       script: [
         { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
       ],
     }
+  },
+  mounted() {
+    console.log('mounted', this.page)
   },
 }
 </script>
