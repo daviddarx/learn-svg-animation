8d3b0a2981c5ac4243241c285d1e7676674d811e
David Darx <david.darx@liip.ch>
2022-04-29 13:27:53 +0200

    feat(about) implement final content

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,36 @@
 <template>
   <article class="about">
-    <h1 class="animate-in">About</h1>
+    <h1 class="animate-in">{{ page.title }}</h1>
+    <section
+      v-for="(content, index) of page.content"
+      :key="'content' + index"
+      class="animate-in"
+      :class="'animate-in--s' + (index + 1)"
+    >
+      <h2 v-if="content.title">{{ content.title }}</h2>
+
+      <p v-html="getHTMLfromMD(content.body)"></p>
+
+      <dl v-if="content.definitionList">
+        <template v-for="list of content.definitionList">
+          <dt :key="list.dlTitle" v-html="list.dlTitle"></dt>
+          <dd :key="list.dlBody" v-html="list.dlBody"></dd>
+        </template>
+      </dl>
+    </section>
   </article>
 </template>
 
 <script>
 export default {
   name: 'AboutPage',
+  async asyncData({ $content }) {
+    const page = await $content('about/about').fetch()
+
+    return {
+      page,
+    }
+  },
   data() {
     return {
       pageTitle: 'About',
@@ -16,6 +40,9 @@ export default {
     return {
       title: `${this.pageTitle} - ${this.headTitleBase}`,
     }
+  },
+  mounted() {
+    console.log(this.page.content)
   },
 }
 </script>
