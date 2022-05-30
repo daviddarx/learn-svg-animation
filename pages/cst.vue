948ea6a2c8367a84ad4e33227c55383e0be80135
David Darx <david.darx@liip.ch>
2022-05-30 22:54:10 +0200

    feat(Cst) change title

--- pages/cst.vue
+++ pages/cst.vue
@@ -2,7 +2,7 @@
   <article class="animation">
     <AppBreadcrumb :title="pageTitle" class="animate-in" />
     <div class="col-left">
-      <h1 class="animate-in animate-in--s1">CST Proof of Concept</h1>
+      <h1 class="animate-in animate-in--s1">CST Test</h1>
     </div>
     <div class="animate-in animate-in--s2">
       <CstMap />
@@ -22,7 +22,7 @@ export default {
   },
   data() {
     return {
-      pageTitle: 'CST Proof of Concept',
+      pageTitle: 'CST Test',
     }
   },
   head() {
